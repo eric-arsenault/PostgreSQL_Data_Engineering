4e4bf014d5d73c136db616443b0278c23f570c57
eric-arsenault <50752216+eric-arsenault@users.noreply.github.com>
2019-12-08 16:31:22 -0700

    update

--- Create_Update_Database.py
+++ Create_Update_Database.py
@@ -1,3 +1,7 @@
+# This is a basic template for a posgreSQL database with a table to store the users.  At the end of the script 
+# I created a function that takes in a username, and creates and new user and stores the users information on 
+# a user table in the database 
+
 from datetime import datetime
 import psycopg2
 
@@ -38,24 +42,28 @@ cur = conn.cursor()
 conn.autocommit = True
 
 #Function to create and store new users 
-def new_user(username):
+def new_user(username, mypassword):
     #create random password
     elements = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'
-    password = str("".join(random.choice(elements) for x in range(10)))
+    new_password = str("".join(random.choice(elements) for x in range(10)))
 
     #create in SQL
-    conn = psycopg2.connect(dbname='internal_users', user='admin', password='psw10203040')
+    conn = psycopg2.connect(dbname='internal_users', user='admin', password=mypassword)
     cur = conn.cursor()
     conn.autocommit = True
-    cur.execute("CREATE USER " + username + " WITH PASSWORD " + "'" + password + "'")  
+    cur.execute("CREATE USER " + username + " WITH PASSWORD " + "'" + new_password + "'")  
     cur.execute("GRANT SELECT ON user_info TO " + username + '"')
-    
+
+    #create user ID 
+    df = pd.read_sql_query("SELECT user_id FROM user_info LIMIT 1 DESC",conn)
+    ID = int(df['user_id'][0]) + 1
+
     #store in SQL
-    cur.execute("INSERT INTO user_info(user_id, username, password, permission, date_created) VALUES " + username + " ," + password + " ," + permission + " ," + datetime.now())
+    cur.execute("INSERT INTO user_info(user_id, username, password, permission, date_created) VALUES " + ID + " ," + username + " ," + password + " ," + "Read" + " ," + datetime.now())
     conn.close()
     
-#create new users
 
+#create new users
 new_user('fakeemail1@gmail.com')
 new_user('fakeemail2@gmail.com')
 new_user('fakeemail3@gmail.com')
