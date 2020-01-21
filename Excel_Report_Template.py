dfeb1fcc2332906831724fe1068156931c1c66dd
Eric Arsenault <50752216+eric-arsenault@users.noreply.github.com>
2020-01-21 13:51:08 -0700

    Update Excel_Report_Template.py

--- Excel_Report_Template.py
+++ Excel_Report_Template.py
@@ -29,90 +29,9 @@ def pull_fix_save_data(system, query, file_name, file_path):
     import datetime
     import pyodbc
     now = datetime.date.today()
-
+        
     if system == "DB1":
-        sql_conn = pyodbc.connect(driver='XXXXX',
-                                server='XXXXX',
-                                database='XXXXX',
-                                trusted_connection='yes')
-        df = pd.read_sql(query, sql_conn)
-        df.fillna("", inplace = True)
-        
-        column_names = [] 
-        for i in df.columns:
-            column_names.append(i.lower())
-        for index, value in enumerate(column_names):
-            if 'login' in value:
-                x = index
-                df2 = df.iloc[:, x].str.split(n = 1, expand = True)
-                df3 = df2.iloc[:, 1].str.split(n = 1, expand = True)
-                df4 = df3.iloc[:, 1].str.split(n = 1, expand = True)
-                
-                d = {'Jan':'1', 'Feb':'2', 'Mar':'3', 'Apr':'4', 'May':'5', 'Jun':'6', 'Jul':'7', 'Aug':'8', 'Sep':'9', 'Oct':'10', 'Nov':'11', 'Dec':'12' }
-                df2[0] = df2[0].map(d)
-                df4[2] = df2[0] + "/" + df3[0] + "/" + df4[0] 
-                df4[2] = df4[2].astype(str)
-                df.drop(df.columns[x],axis=1,inplace=True)
-
-                new_last_login = []
-                for i in df4[2]:
-                    if str(i) == 'nan':
-                        new_last_login.append("")
-                    elif str(i) == "":
-                        new_last_login.append("")
-                    else:new_last_login.append(str(datetime.datetime.strptime(i, '%m/%d/%Y').date()))
-                
-                df["Last Login"] = new_last_login
-        
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-            else: 
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-
-        for index, value in enumerate(column_names):
-            if 'create' in value:
-                y = index
-                df.iloc[:, y]  = df.iloc[:, y].astype(str)
-                df5 = df.iloc[:, y].str.split("-", expand = True) 
-                df6 = df5[2].str.split(" ", expand = True)
-                df6[2] = df5[1] + '/' + df6[0] + '/' + df5[0] 
-                
-                new_create_date = []
-                for i in df6[2]:
-                    if str(i) == 'nan':
-                        new_create_date.append("")
-                    elif str(i) == "":
-                        new_create_date.append("")
-                    else: new_create_date.append(str(datetime.datetime.strptime(i, '%m/%d/%Y').date()))
-
-                df.drop(df.columns[y],axis=1,inplace=True)
-                df['Date Created'] = new_create_date
-
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-            else:                   
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-
-    elif system == "DB2":
-        sql_conn = pyodbc.connect(driver='XXXXX',
+        sql_conn = pyodbc.connect(driver='{SQL Server Native Client 11.0}',
                                     server='XXXXX',
                                     database='XXXXX',
                                     trusted_connection='yes')
@@ -141,21 +60,7 @@ def pull_fix_save_data(system, query, file_name, file_path):
                 
                 df.drop(df.columns[x],axis=1,inplace=True)
                 df['Last Login'] = new_last_login
-
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-            else:                   
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-       
+        
         for index, value in enumerate(column_names):
             if 'create' in value:
                 y = index
@@ -175,21 +80,74 @@ def pull_fix_save_data(system, query, file_name, file_path):
                 df.drop(df.columns[y],axis=1,inplace=True)
                 df["Date Created"] = new_create_date 
 
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx')
-            else:                   
-                openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
-                wb = Workbook()
-                ws = wb.active
-                for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
-                os.chdir(file_path)
-                wb.save(file_name + "_" + str(now) + '.xlsx') 
-    
-             
-pull_fix_save_data(system, query, file_name, file_path)  
+        openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
+        wb = Workbook()
+        ws = wb.active
+        for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
+        os.chdir(file_path)
+        wb.save(file_name + "_" + str(now) + '.xlsx')
+
+    elif system == "DB2":
+        sql_conn = pyodbc.connect(driver='{SQL Server Native Client 11.0}',
+                                server='XXXXX',
+                                database='XXXXX',
+                                trusted_connection='yes')
+        df = pd.read_sql(query, sql_conn)
+        df.fillna("", inplace = True)
+
+        column_names = [] 
+        for i in df.columns:
+            column_names.append(i.lower())
+        
+        for index, value in enumerate(column_names):
+            if "login" in value:
+                x = index
+                #do not split by (" ")  <- data entry teams sometimes add an extra space
+                df2 = df.iloc[:, x].str.split(n = 1, expand = True)
+                df3 = df2.iloc[:, 1].str.split(n = 1, expand = True)
+                df4 = df3.iloc[:, 1].str.split(n = 1, expand = True)
+                d = {'Jan':'1', 'Feb':'2', 'Mar':'3', 'Apr':'4', 'May':'5', 'Jun':'6', 'Jul':'7', 'Aug':'8', 'Sep':'9', 'Oct':'10', 'Nov':'11', 'Dec':'12' }
+                df2[0] = df2[0].map(d)
+                df2[4] = df2[0] + "/" + df3[0] + "/" + df4[0] 
+                df2[4] = df2[4].astype(str)
+                df.drop(df.columns[x],axis=1,inplace=True)
+
+                new_last_login = []
+                for i in df2[4]:
+                    if str(i) == 'nan':
+                        new_last_login.append("")
+                    elif str(i) == "":
+                        new_last_login.append("")
+                    else:new_last_login.append(str(datetime.datetime.strptime(i, '%m/%d/%Y').date()))
+                    
+                df["Last Login"] = new_last_login
+
+        for index, value in enumerate(column_names):
+            if 'create' in value:
+                y = index
+                df.iloc[:, y]  = df.iloc[:, y].astype(str)
+                df5 = df.iloc[:, y].str.split("-", expand = True) 
+                df6 = df5[2].str.split(" ", expand = True)
+                df6[2] = df5[1] + '/' + df6[0] + '/' + df5[0] 
+                
+                new_create_date = []
+                for i in df6[2]:
+                    if str(i) == 'nan':
+                        new_create_date.append("")
+                    elif str(i) == "":
+                        new_create_date.append("")
+                    else: new_create_date.append(str(datetime.datetime.strptime(i, '%m/%d/%Y').date()))
+
+                df.drop(df.columns[y],axis=1,inplace=True)
+                df['Date Created'] = new_create_date
+
+        openpyxl.utils.dataframe.dataframe_to_rows(df, index=False, header=True)
+        wb = Workbook()
+        ws = wb.active
+        for row in dataframe_to_rows(df, index=False, header=True) : ws.append(row)
+        os.chdir(file_path)
+        wb.save(file_name + "_" + str(now) + '.xlsx')
+
+pull_fix_save_data(system, query, file_name, file_path)
 
             
